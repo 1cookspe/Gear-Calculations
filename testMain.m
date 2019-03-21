9b582f747eb4a93f6f8c6707339a8db7168b62fe
Spencer Cook <spencercook@Spencers-MacBook-Pro.local>
2019-03-21 19:32:26 -0400

    Testing working...

--- testMain.m
+++ testMain.m
@@ -1,4 +1,10 @@
 pitchAngle = 20;
 pinionTeeth = 22;
 gearTeeth = 60;
+diametral = 4;
 width = 3.25;
+Qv = 6;
+St = 32125;
+Sc = 109600;
+
+[bendStress, bendEndurance, bendFOS, contactStress, wearEnd, wearFOS] = testingFunction(pinionTeeth,gearTeeth,pinionTeeth,gearTeeth,diametral,diametral,Qv,width,0,0,0,0,0,0,0,0,0.35,0.41,0.35,0.41,pitchAngle,0.331,0.422,0.331,0.422)
